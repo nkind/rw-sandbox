939fb26ac36e29806a99f83ec9e90f4414506f04
nkind <nkindrachuk@gmail.com>
2024-04-07 15:16:26 -0600

    disable signup

--- api/src/functions/auth.js
+++ api/src/functions/auth.js
@@ -112,20 +112,8 @@ export const handler = async (event, context) => {
     //
     // If this returns anything else, it will be returned by the
     // `signUp()` function in the form of: `{ message: 'String here' }`.
-    handler: ({
-      username,
-      hashedPassword,
-      salt,
-      userAttributes: _userAttributes,
-    }) => {
-      return db.user.create({
-        data: {
-          email: username,
-          hashedPassword: hashedPassword,
-          salt: salt,
-          // name: userAttributes.name
-        },
-      })
+    handler: () => {
+      return false
     },
 
     // Include any format checks for password here. Return `true` if the
